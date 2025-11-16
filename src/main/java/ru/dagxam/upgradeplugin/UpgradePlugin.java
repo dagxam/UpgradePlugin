ed0f25900bee6e2d30be8219f09a6cc7081ecbee
dagxam <dagxam@gmail.com>
2025-11-16 20:53:08 +0300

    Update UpgradePlugin.java

--- src/main/java/ru/dagxam/upgradeplugin/UpgradePlugin.java
+++ src/main/java/ru/dagxam/upgradeplugin/UpgradePlugin.java
@@ -1,10 +1,9 @@
-// ИЗМЕНЕНО
 package ru.dagxam.upgradeplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
-// ИЗМЕНЕНО
 import ru.dagxam.upgradeplugin.commands.GiveBookCommand;
 import ru.dagxam.upgradeplugin.listeners.AnvilListener;
+import ru.dagxam.upgradeplugin.listeners.BlockBreakListener; // <-- ДОБАВЬТЕ ЭТОТ ИМПОРТ
 import ru.dagxam.upgradeplugin.listeners.VillagerListener;
 
 public final class UpgradePlugin extends JavaPlugin {
@@ -13,12 +12,13 @@ public final class UpgradePlugin extends JavaPlugin {
     public void onEnable() {
         getLogger().info("Инициализация ItemManager...");
         
-        // 1. Регистрация команд
         getCommand("giveupgradebook").setExecutor(new GiveBookCommand());
 
-        // 2. Регистрация слушателей событий
-        getServer().getPluginManager().registerEvents(new AnvilListener(this), this); // Передаем 'this'
-        getServer().getPluginManager().registerEvents(new VillagerListener(this), this); // Передаем 'this'
+        // Регистрация слушателей
+        getServer().getPluginManager().registerEvents(new AnvilListener(this), this);
+        getServer().getPluginManager().registerEvents(new VillagerListener(this), this);
+        // --- ДОБАВЬТЕ ЭТУ СТРОКУ ---
+        getServer().getPluginManager().registerEvents(new BlockBreakListener(), this); 
 
         getLogger().info("UpgradePlugin успешно включен!");
     }
